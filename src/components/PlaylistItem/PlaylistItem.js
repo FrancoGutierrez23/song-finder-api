d0e0a4c70ac481484824e10cbdd6a2e2a18a8371
Franco Gutierrez <francogutierre523523@gmail.com>
2024-05-28 10:53:42 -0300

    Change style of the volume control and make it dinamic, give the option of edit the playlist name, add options button for the PlaylistItem

--- src/components/PlaylistItem/PlaylistItem.js
+++ src/components/PlaylistItem/PlaylistItem.js
@@ -1,26 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './PlaylistItem.css';
 import SongPreviewButton from '../SongPreviewButton/SongPreviewButton';
+import { PiMusicNotesMinusFill } from "react-icons/pi";
 
 const PlaylistItem = ({song, removeFromPlaylist, playlist, style}) => {
   const [isRemoving, setIsRemoving] = useState(false);
+  const [showOptions, setShowOptions] = useState(false); // State to control options visibility
+  const optionsRef = useRef(null); // Ref for the options container
 
   const handleClick = () => {
     setIsRemoving(true); // Trigger the removal animation
     setTimeout(() => removeFromPlaylist(playlist.indexOf(song)), 200); // Remove the song after the animation
   };
-  
+
+  const toggleOptions = () => setShowOptions(prev => !prev); // Toggle options visibility
+
+  // Effect to handle clicks outside of the options container
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (optionsRef.current && !optionsRef.current.contains(event.target)) {
+        setShowOptions(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+  }
+
   return (
-    <div className={`added-card ${isRemoving ? 'removing' : (isRemoving === false ? 'adding' : '')}`} style={style}>
+    <div className={`added-card ${isRemoving ? 'removing' : ''}`} style={style}>
       <div className='headings' >
-        <h3>{song.title}</h3>
+      <h3>{song.title}<p className='duration'> {formatTime(song.duration)}</p></h3>
         <div className='cont'>{`${song.artist.name} at ${song.album.title}`}</div>
       </div>
       <div className='card-bttns'>
         <SongPreviewButton previewUrl={song.preview} />
-      <button onClick={handleClick} className='remove-btn'>x</button>
+        <button onClick={toggleOptions} className='options-btn'>⋮</button>
+        {showOptions && (
+          <div ref={optionsRef}>
+            <button onClick={handleClick} className='remove-btn'><PiMusicNotesMinusFill style={{fontSize: '1.3em', color: '#7a1717'}} />Remove</button>
+          </div>
+        )}
       </div>
-      
     </div>
   );
 };
--- src/components/VolumeControl/VolumeControl.js
+++ src/components/VolumeControl/VolumeControl.js
@@ -1,31 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import './VolumeControl.css';
+import { Range, getTrackBackground } from 'react-range';
+import { RiVolumeMuteFill, RiVolumeDownFill, RiVolumeUpFill } from "react-icons/ri";
 
 const VolumeControl = () => {
   const [volume, setVolume] = useState(0.5);
 
-  const handleVolumeChange = (e) => {
-    const newVolume = parseFloat(e.target.value);
+  const handleVolumeChange = (values) => {
+    const newVolume = values[0] / 100;
     setVolume(newVolume);
-    // Update volume of all audio elements
     const audioElements = document.querySelectorAll('audio');
-    audioElements.forEach((audio) => {
+    audioElements.forEach(audio => {
       audio.volume = newVolume;
     });
   };
 
+  const volumeIcon = useMemo(() => {
+    if (volume === 0) {
+      return <RiVolumeMuteFill />;
+    } else if (volume > 0.5) {
+      return <RiVolumeUpFill />;
+    } else {
+      return <RiVolumeDownFill />;
+    }
+  }, [volume]);
+
   return (
     <div className='volume-control-container'>
-      <input
-        type="range"
-        min="0"
-        max="1"
-        step="0.01"
-        value={volume}
+      <Range
+        values={[volume * 100]}
+        step={1}
+        min={0}
+        max={100}
         onChange={handleVolumeChange}
+        renderTrack={({ props, children }) => {
+          // Extract key from props
+          const { key, ...restProps } = props;
+          return (
+            <div
+              key={key} // Apply key directly here
+              {...restProps}
+              style={{
+                ...restProps.style,
+                height: '8px',
+                width: '100%',
+                background: getTrackBackground({
+                  values: [volume * 100],
+                  colors: ['#fea500', '#ccc'],
+                  min: 0,
+                  max: 100
+                })
+              }}
+              className="track"
+            >
+              {children}
+            </div>
+          );
+        }}
+        renderThumb={({ props }) => {
+          // Extract key from props
+          const { key, ...restProps } = props;
+          return (
+            <div
+              key={key} // Apply key directly here
+              {...restProps}
+              className="thumb"
+            >
+              {volumeIcon}
+            </div>
+          );
+        }}
       />
     </div>
   );
 };
 
 export default VolumeControl;
+
